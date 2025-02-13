337c38c2e8fa99276922b7ed152606e3b50dc166
asecer79 <asecer79@DESKTOP-TPOST9I>
2025-02-13 12:11:25 +0300

    Add project files.

---
+++ Week1_2/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week1_2.Models;
+
+namespace Week1_2.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        //Actions
+
+        public ActionResult Sum()
+        {
+            ViewBag.sumResult = 5 + 6;
+            ViewBag.name = "Aydın";
+
+          
+
+            return View();
+        }
+
+        public ActionResult StudentList(int id,string name)
+        {
+
+            name = name ?? "";
+
+
+
+            var student1 = new Student()
+            {
+                Id = 1,
+                Name = "Ayşe",
+                StudentId = "125345113",
+                Description = "sadasd"
+            };
+
+            var student2 = new Student()
+            {
+                Id = 2,
+                Name = "Mahmut",
+                StudentId = "12534511w3",
+                Description = "sadasd"
+            };
+            var student3 = new Student()
+            {
+                Id = 3,
+                Name = "Neşecan",
+                StudentId = "125s345w113",
+                Description = "sadasd"
+            };
+
+            var students = new List<Student>()
+            {
+                student1,
+                student2,
+                student3
+            };
+
+            students = students.Where(
+                p => 
+                    p.Id == id &&
+                    p.Name.Contains(name)
+                    
+                    
+                    ).ToList();
+
+            //students.Add(student1);
+            //students.Add(student2);
+            //students.Add(student3);
+
+            return View(students);
+        }
+
+     
+    }
+}
