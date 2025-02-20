a02950c295f9ca97e938b15f4a117e03aecd90df
asecer79 <asecer79@DESKTOP-TPOST9I>
2025-02-20 11:26:30 +0300

    u

---
+++ Week3/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week3.Models;
+
+namespace Week3.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var data = SchoolDb.StudentsTable;
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var student = new Student();
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            SchoolDb.StudentsTable.Add(student);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var student = SchoolDb.StudentsTable.FirstOrDefault(p=>p.Id==id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student student)
+        {
+           var updated= SchoolDb.StudentsTable.FirstOrDefault(p=>p.Id== student.Id);
+           updated.Description=student.Description;
+           updated.Name=student.Name;
+           updated.StudentId=student.StudentId;
+
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var remove = SchoolDb.StudentsTable.FirstOrDefault(p => p.Id == id);
+
+            SchoolDb.StudentsTable.Remove(remove);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Week3/Models/SchoolDb.cs
@@ -0,0 +1,42 @@
+﻿namespace Week3.Models
+{
+    public class SchoolDb
+    {
+        public static List<Student> StudentsTable { get; set; } = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                Name = "Ayşe Dalman",
+                StudentId = "8965435123",
+            },
+            new Student()
+            {
+                Id = 2,
+                Name = "Ahmet Salman",
+                StudentId = "678435123",
+            },
+            new Student()
+            {
+                Id = 3,
+                Name = "Mustafa Baykara",
+                StudentId = "234435123",
+            },
+            new Student()
+            {
+                Id = 4,
+                Name = "Mehmet Karaca",
+                StudentId = "7895435123",
+            },
+            new Student()
+            {
+                Id = 5,
+                Name = "Mahmut Yılmaz",
+                StudentId = "12345435123",
+            },
+
+
+        };
+        
+    }
+}
