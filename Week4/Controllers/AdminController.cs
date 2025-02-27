3252770c4967ba91f6696ce5ba7aceb5aac1ec64
asecer79 <asecer79@DESKTOP-TPOST9I>
2025-02-27 11:52:53 +0300

    u

---
+++ Week4/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week4.Controllers
+{
+
+    //multiple route
+
+    [Route("security")]
+    public class AdminController : Controller
+    {
+
+        [HttpGet("p1/{id:int?}")]
+        [HttpGet("p2")]
+        [HttpGet("p3")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
