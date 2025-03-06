c3469489b8816d8ecf0bc0a6143a2d1f112336ae
asecer79 <asecer79@DESKTOP-TPOST9I>
2025-03-06 11:51:57 +0300

    u

--- Week5/Controllers/ProductsController.cs
+++ Week5/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Week5.Data;
+using Week5.Models.Entities;
 
 namespace Week5.Controllers
 {
@@ -18,6 +19,31 @@ namespace Week5.Controllers
 
 
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var product = new Product();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);  //INSERT INTO Products (Id, Name, Decription, Price, Brand) VALUES (.,,.,,)
+                db.SaveChanges();
+
+                return RedirectToAction("Index","Products");
+            }
+
+
+
+            return View(product);
         }
     }
 }
